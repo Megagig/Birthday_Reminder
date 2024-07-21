7cfb679b17e7b870ab357f610dd484fb5408afe5
Megagig <megagigsoftwaresolutions.com>
2024-07-21 18:24:30 +0100

    Iterate and Render List

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import { useState } from 'react';
+import data from './data';
 const App = () => {
-  return <h2>Birthday Reminder </h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <h2> 5 Birthdays Today</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </>
+  );
 };
 export default App;
